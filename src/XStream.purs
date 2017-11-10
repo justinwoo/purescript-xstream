f73cc3bb639b059a53c0cb172912eb204d4b91e1
justinwoo <moomoowoo@gmail.com>
2017-11-10 18:33:06 +0200

    update dependencies

--- src/XStream.purs
+++ src/XStream.purs
@@ -22,7 +22,6 @@ module Control.XStream
   , flattenEff
   , fold
   , fromCallback
-  , fromAff
   , fromArray
   , imitate
   , last
@@ -43,17 +42,13 @@ module Control.XStream
 
 import Prelude
 import Control.Alt (class Alt)
-import Control.Monad.Aff (cancel, runAff, Aff)
 import Control.Monad.Eff (Eff, kind Effect)
-import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (CONSOLE, log)
-import Control.Monad.Eff.Exception (error, Error, try, message)
-import Control.Monad.Eff.Ref (REF, readRef, writeRef, newRef)
+import Control.Monad.Eff.Exception (Error, message, try)
 import Control.Monad.Eff.Timer (TIMER)
 import Control.Plus (class Plus)
 import Data.Either (Either)
 import Data.Function.Uncurried (Fn2, Fn3, runFn2, runFn3)
-import Data.Maybe (Maybe(Just, Nothing))
 import Data.Monoid (class Monoid)
 
 foreign import data Stream :: Type -> Type
@@ -185,26 +180,6 @@ fromCallback cb =
   create
     { start: \l -> void $ cb l.next
     , stop: const $ pure unit
-    }
-
--- | create a `Stream` from an Aff
-fromAff :: forall a e. Aff (stream :: STREAM, ref :: REF | e) a -> Eff (stream :: STREAM, ref :: REF | e) (Stream a)
-fromAff aff = do
-  ref <- newRef Nothing
-  create
-    { start: \l -> do
-        canceler <- runAff
-          l.error
-          (\a -> do
-            l.next a
-            l.complete unit)
-          aff
-        liftEff $ writeRef ref $ Just canceler
-    , stop: \_ -> do
-        mRef <- readRef ref
-        case mRef of
-          Just c -> void $ runAff (const $ pure unit) (const $ pure unit) $ cancel c (error "Unsubscribed")
-          Nothing -> pure unit
     }
 
 foreign import _addListener :: forall e a. Fn2 (Listener e a) (Stream a) (EffS e Unit)
